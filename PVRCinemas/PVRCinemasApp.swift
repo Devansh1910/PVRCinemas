91f2da3f76d5dee389fec41e140e80df0a775ebf
Devansh Saxena <devanshsaxean1019@gmail.com>
2025-01-14 02:31:03 +0530

    Initial Commit

---
+++ PVRCinemas/PVRCinemasApp.swift
@@ -0,0 +1,17 @@
+//
+//  PVRCinemasApp.swift
+//  PVRCinemas
+//
+//  Created by Devansh Saxena on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct PVRCinemasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
